8c2c8dd4c81c9ff81ff633dfc4bfee0a95416ab7
PHINXED <luisgg099@gmail.com>
2026-01-14 13:07:13 +0100

    carta bares

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "https://TU-PROYECTO.supabase.co",
-  "TU_PUBLIC_ANON_KEY"
+  "https://qozzxdrjwjskmwmxscqj.supabase.co",
+  "sb_publishable_UtBfupVv8e2zniYdv1jvEA_SXIyWE0Z"
 );
